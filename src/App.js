8b71f8fae1dc2089e42a52ad19d9ebbac7c1a1e3
ranjith <ranjithch137@gmail.com>
2021-10-14 11:43:08 +0530

    k

--- src/App.js
+++ src/App.js
@@ -286,37 +286,37 @@ function Login() {
     <Container className="container">
       <Row>
         <Col xs='auto' sm='7' md='6' lg='4'>
-          {message === "waiting" ?
+        {message === "waiting" ?
             <Spinner animation="border" role="status">
               <span className="visually-hidden">Loading...</span>
             </Spinner>
             :
-            (message ? message :
-              <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>
-                    <span className="error">*</span>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>
-                    <span className="error">*</span>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
-                </Form.Group>
-                <p className="error">
-                  {error}
-                </p>
-                <Button variant="success" type="submit">
-                  Login
-                </Button><br />
-                <Link to="/Forgot" className="link">
-                  Forgot password?
-                </Link><br />
-                <Button variant="primary" className="centre-button" onClick={() => history.push('/SignUp')}>
-                  Create account
-                </Button>
-              </Form>
-            )
+          (message ? message :
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>
+                  <span className="error">*</span>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>
+                  <span className="error">*</span>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+              </Form.Group>
+              <p className="error">
+                {error}
+              </p>
+              <Button variant="success" type="submit">
+                Login
+              </Button><br />
+              <Link to="/Forgot" className="link">
+                Forgot password?
+              </Link><br />
+              <Button variant="primary" className="centre-button" onClick={() => history.push('/SignUp')}>
+                Create account
+              </Button>
+            </Form>
+  )
           }
         </Col>
       </Row>
